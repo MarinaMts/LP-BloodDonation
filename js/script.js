60e6a553e5937208bede40a8e64deb975c18af6b
Pedro Henrique de Sousa <pedro.sousa21@outlook.com>
2023-05-15 18:02:46 -0300

    Adicionando fontes e padronizando layout

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
