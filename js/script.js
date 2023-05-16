0d135a549aff488bd0fa8b5cf6d3ca18e1739914
gabrielmoraiis <bielmorais2004@gmail.com>
2023-05-16 17:06:22 -0300

    Scripts adicionados, cores e fontes alteradas.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
+function onScroll() {
+  const nav = document.querySelector("nav");
+  if (window.scrollY > 0) {
+    nav.classList.add("scroll");
+  } else {
+    nav.classList.remove("scroll");
+  }
+}
+
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
-  centerSlide: 'true',
-  fade: 'true',
-  grabCursor: 'true',
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -15,3 +24,11 @@ var swiper = new Swiper(".slide-content", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  durantion: 700,
+}).reveal(
+  "#cells .title, #boxes, #boxes .donation-title, #boxes .all-donations, #declaration, #declaration h1, #declaration .container, #information, #information .div1, #information .div2, #information .div3, #information .div4, #agende, #agende h2,#agende div, #agende #developer"
+);
