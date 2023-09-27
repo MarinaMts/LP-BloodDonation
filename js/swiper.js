8fb10e7dbc0d5a0390795e9d3bb23d7e9033ec82
Marina Martins <marina.martinss_@outlook.com>
2023-09-27 17:38:25 -0300

    Add pagina depoimentos
    Criação de nova página e formulário em modal

---
+++ js/swiper.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper('.slide-content', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
+  
+  ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    durantion: 700
+  }).reveal(
+    '#cells .title, #boxes, #boxes .donation-title, #boxes .all-donations, #declaration, #declaration h1, #declaration .container, #information, #information .div1, #information .div2, #information .div3, #information .div4, #agende, #agende h2,#agende div, #agende #developer'
+  );
+  
+  
